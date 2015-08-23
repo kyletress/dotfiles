758721a252b2bfcbd4c005fc583b2864d47a5172
Kyle Tress <kyle@kyletress.com>
2015-08-23 15:34:49 -0400

    Add atom config files and update osx.sh and bash_profile

--- osx.sh
+++ osx.sh
@@ -3,12 +3,20 @@
 # set computer name
 sudo scutil --set ComputerName "Tress"
 
-# set up Safari
+# Safari
+
 # Include the develop menu
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 # Set the home page to blank
 defaults write com.apple.Safari HomePage -string "about:blank"
 
+# Finder
+
 # Show hidden files and file extensions by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Dock
+
+# Auto hide
+defaults write com.apple.dock autohide -bool true
