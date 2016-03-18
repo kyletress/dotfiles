56f1f3e0f9cf0bc081629c728a07f59417c41aca
Kyle Tress <kyle@kyletress.com>
2016-03-18 16:43:51 -0400

    Add sledsheet alias

--- osx.sh
+++ osx.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+echo 'Setting up your OS X preferences'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # set computer name
-sudo scutil --set ComputerName "Tress"
+sudo scutil --set ComputerName "tress"
 
 # Safari
 
@@ -26,3 +34,14 @@ defaults write com.apple.dock autohide -bool true
 
 # Disable screenshot shadows
 defaults write com.apple.screencapture disable-shadow -bool TRUE
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
